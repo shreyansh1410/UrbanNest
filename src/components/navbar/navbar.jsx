b08903f2555c116db1e2b1def2f1d431088ad443
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-04-13 00:30:02 +0530

    responsive navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="navbar">
       <div className="left">
-        <a href="/">
-          <img src= "/logo.png"></img>
-          <span>Lama Estate</span>
+        <a href="/" className="logo">
+          <img src="/logo.png"></img>
+          <span className="logotext">Urban Nest</span>
         </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
       </div>
-      <div className="right">Right</div>
+
+      <div className="right">
+        <a href="/" className="">
+          Sign In
+        </a>
+        <a href="/" className="signup">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="hamburger" onClick={() => {setIsMenuOpen(!isMenuOpen)}}/>
+        </div>
+        <div className={isMenuOpen? "menuItems active" : "menuItems"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
     </div>
   );
 };
