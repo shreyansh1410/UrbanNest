767314f6f388fede9ddbb7ec6d7c5690bd7a4da9
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-04-16 13:13:56 +0530

    added newpost, register and profileupdate pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/navbar/navbar";
 import HomePage from "./routes/homePage/HomePage";
-
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,43 +11,43 @@ import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/singlePage/SinglePage";
 import Login from "./routes/Login/Login";
 import ProfilePage from "./routes/ProfilePage/ProfilePage";
+import RegisterPage from "./routes/Register/Regsiter";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
-      <Layout/>,
-      children:[
-        {
-          path: "/",
-          element: <HomePage/>
-        },
-        {
-          path: "/list",
-          element: <ListPage/>
-        },
-        {
-          path: "/:id",
-          element: <SinglePage/>
-        },
-        {
-          path: "/login",
-          element: <Login/>
-        },
-        {
-          path: "/profile",
-          element: <ProfilePage/>
-        }
-      ]
-  }
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/list",
+        element: <ListPage />,
+      },
+      {
+        path: "/:id",
+        element: <SinglePage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-  return (
-    
-
-    <RouterProvider router={router}/>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
