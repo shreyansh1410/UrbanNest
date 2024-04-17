8321d870d9883e111ad7e0f76537d4b506e2af96
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-04-17 21:16:33 +0530

    solved cookieparser issue

--- api/app.js
+++ api/app.js
@@ -2,15 +2,14 @@ import express from "express";
 import cors from "cors";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
-// app.use(cookieParser);
+app.use(cookieParser());
 
-// app.uesr(cors({origin:}))
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
