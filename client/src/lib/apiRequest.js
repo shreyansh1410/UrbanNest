5c588b48630ade446284c7ceb8e69e338d3c899e
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-04-22 18:58:11 +0530

    added backend link

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8800/api",
-  baseURL: "https://urbannest-backend-6jz4.onrender.com//api",
+  baseURL: "https://urbannest-backend-6jz4.onrender.com/api",
   withCredentials: true,
 });
 
